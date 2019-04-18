da90027429a7e2d72c02948a69bea7da001ca107
Antony <A.Krutenok@gmail.com>
2019-04-18 15:54:55 +0300

    Updates

---
+++ precache-manifest.5440839e08bd90c4a27927f3fbd92e90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "91b1ad5b54666565985c2793e6fb5637",
+    "url": "/todo/static/media/logo.91b1ad5b.png"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  },
+  {
+    "revision": "a09c60437aba4b96370e",
+    "url": "/todo/static/js/main.0b7a572b.chunk.js"
+  },
+  {
+    "revision": "0ab4a87fbb9d956e0ecf",
+    "url": "/todo/static/js/2.462483ac.chunk.js"
+  },
+  {
+    "revision": "a09c60437aba4b96370e",
+    "url": "/todo/static/css/main.3efc5e6b.chunk.css"
+  },
+  {
+    "revision": "c600f40e68e1226c0392da5c522ef563",
+    "url": "/todo/index.html"
+  }
+];
